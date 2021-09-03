72ee873f614ecbc2b2af05936cfee699135d6ac2
muchlist <whois.muchlis@gmail.com>
2021-09-03 20:30:57 +0800

    gracefully shutdown

--- app/app.go
+++ app/app.go
@@ -1,9 +1,14 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"github.com/muchlist/berita_acara/configs"
 	"github.com/muchlist/berita_acara/db"
 	"github.com/muchlist/berita_acara/utils/logger"
+	"log"
+	"os"
+	"os/signal"
 )
 
 func RunApp(){
@@ -11,8 +16,28 @@ func RunApp(){
 	configs.Init()
 	logger.Init()
 	db.Init()
-	defer db.Close()
 
+	// create fiber app
+	app := fiber.New()
 
+	// gracefully shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		_ = <-c
+		fmt.Println("Gracefully shutting down...")
+		_ = app.Shutdown()
+	}()
 
+	// ...
+
+	// blocking and listen for fiber
+	if err := app.Listen(":3500"); err != nil {
+		logger.Error("error fiber listen", err)
+		log.Panic()
+	}
+
+	// cleaning up app, ex db close and etc
+	fmt.Println("Running cleanup tasks...")
+	db.Close()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/muchlist/berita_acara
 go 1.16
 
 require (
+	github.com/gofiber/fiber/v2 v2.18.0 // indirect
 	github.com/jackc/pgx/v4 v4.13.0
 	github.com/joho/godotenv v1.3.0
 	go.uber.org/zap v1.19.0
